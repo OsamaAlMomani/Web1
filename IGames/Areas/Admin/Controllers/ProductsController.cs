5f7c818de47181ecbc352eb93dbcf2eb8689e809
OsamaAlMomani <momani.322.44157@gmail.com>
2022-12-07 10:56:38 +0300

    small changes 12/7

---
+++ IGames/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.Data.ApplicationDbContext;
+using RModel.Models;
+using IGames.DataInterface;
+
+namespace IGames.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly product _context;
+
+        public ProductsController(product context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Products
+        public async Task<IActionResult> Index()
+        {
+            var product = _context.Product.Include(p => p.category_R).Include(p => p.coverType);
+            return View(await product.ToListAsync());
+        }
+
+        // GET: Admin/Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.category_R)
+                .Include(p => p.coverType)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Products/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Set<Category>(), "ID", "Name");
+            ViewData["CoverTypeID"] = new SelectList(_context.Set<CoverType>(), "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Title,ISBN,Author,Description,CategoryId,Price,Price50,Price100,ImageUrl,CoverTypeID")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Set<Category>(), "ID", "Name", product.CategoryId);
+            ViewData["CoverTypeID"] = new SelectList(_context.Set<CoverType>(), "Id", "Name", product.CoverTypeID);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Set<Category>(), "ID", "Name", product.CategoryId);
+            ViewData["CoverTypeID"] = new SelectList(_context.Set<CoverType>(), "Id", "Name", product.CoverTypeID);
+            return View(product);
+        }
+
+        // POST: Admin/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Title,ISBN,Author,Description,CategoryId,Price,Price50,Price100,ImageUrl,CoverTypeID")] Product product)
+        {
+            if (id != product.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Set<Category>(), "ID", "Name", product.CategoryId);
+            ViewData["CoverTypeID"] = new SelectList(_context.Set<CoverType>(), "Id", "Name", product.CoverTypeID);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.category_R)
+                .Include(p => p.coverType)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Product == null)
+            {
+                return Problem("Entity set 'product.Product'  is null.");
+            }
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.Product?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ IGames/Areas/Admin/Data/product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RModel.Models;
+
+namespace DataAccess.Data.ApplicationDbContext
+{
+    public class product : DbContext
+    {
+        public product (DbContextOptions<product> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RModel.Models.Product> Product { get; set; } = default!;
+    }
+}
